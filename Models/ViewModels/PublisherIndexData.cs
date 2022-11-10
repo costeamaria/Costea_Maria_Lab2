be8ae035efec52759a045a7702a374228720d62d
Maria Costea <costea.mari23@gmail.com>
2022-11-10 12:27:21 +0200

    Filtrarea rezultatelor afisate conform search string-ului

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,9 @@
+﻿namespace Costea_Maria_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
